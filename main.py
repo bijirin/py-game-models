a270995bf3c6f63e22f071b415f2873302788332
bijirin <tsutrisn@gmail.com>
2026-05-16 23:05:13 +0700

    Implement game database

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players = json.load(file)
+
+    for key, value in players.items():
+
+        player_race, created = Race.objects.get_or_create(
+            name=value["race"]["name"],
+            description=value["race"]["description"],
+        )
+
+        for skill in value["race"]["skills"]:
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=player_race,
+            )
+
+        if value["guild"] is None:
+            player_guild = None
+        else:
+            player_guild, created = Guild.objects.get_or_create(
+                name=value["guild"]["name"],
+                description=value["guild"]["description"],
+            )
+
+        Player.objects.get_or_create(
+            nickname=key,
+            email=value["email"],
+            bio=value["bio"],
+            race=player_race,
+            guild=player_guild,
+        )
 
 
 if __name__ == "__main__":
